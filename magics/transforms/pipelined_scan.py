b81b58c5e8e5c89294fc92896b9864f45bfcb863
Yunlong Liu <yliu120@users.noreply.github.com>
2025-06-11 21:42:05 -0700

    Update pipelined_scan.py

--- magics/transforms/pipelined_scan.py
+++ magics/transforms/pipelined_scan.py
@@ -5,8 +5,8 @@ from typing import Callable, TypeVar
 import jax
 import jax.numpy as jnp
 
-from jax_examples.magics.ops import allocate_buffer, annotate_memory_space
-from jax_examples.magics.transforms.utils import replace_pytree_leaves
+from xlm2.jax.ops import allocate_buffer, annotate_memory_space
+from xlm2.jax.transforms.utils import replace_pytree_leaves
 
 Carry = TypeVar("Carry")
 X = TypeVar("X")
@@ -16,24 +16,29 @@ BodyFn = Callable[[Carry, X], tuple[Carry, Y]]
 
 # The carry pytree has the following fields:
 # - The original passed by user.
-# - The index of the current layer. (Additional field for pipelining)
+# - The index for slicing the residuals on host in current layer.
+#   (Important: Please keep the current way of slicing because it is important performance-wise)
 # - The host storage for residuals. (Additional field for pipelining)
 # - The VJP pytree that we passes to the next iteration. (Additional field for pipelining)
 WrappedCarry = namedtuple(
-    "WrappedCarry", ["carry", "layer_idx", "residuals_on_host", "vjp_fn"]
+    "WrappedCarry", ["carry", "slice_idx", "residuals_on_host", "vjp_fn"]
 )
 
 
 def fwd_body(c: WrappedCarry, x: jax.tree_util.PyTreeDef, fun: BodyFn = None):
     assert fun is not None
-    out, layer_idx, residuals_on_host, prev_vjp_fn = c
-    residuals_on_host = jax.tree.map(
-        lambda x, y: annotate_memory_space(
+    out, slice_idx, residuals_on_host, prev_vjp_fn = c
+
+    def _memcpy_to_host(x, y):
+        return annotate_memory_space(
             jax.lax.dynamic_update_slice_in_dim(
-                x, jnp.expand_dims(y, axis=0), layer_idx - 1, axis=0
+                x, y, slice_idx, axis=0, allow_negative_indices=False
             ),
             memory_space="host",
-        ),
+        )
+
+    residuals_on_host = jax.tree.map(
+        lambda x, y: _memcpy_to_host(x, jnp.expand_dims(y, axis=0)),
         residuals_on_host,
         prev_vjp_fn,
     )
@@ -41,23 +46,36 @@ def fwd_body(c: WrappedCarry, x: jax.tree_util.PyTreeDef, fun: BodyFn = None):
     new_vjp_fn = replace_pytree_leaves(prev_vjp_fn, vjp_fn)
     return WrappedCarry(
         carry=out,
-        layer_idx=layer_idx + 1,
+        slice_idx=slice_idx + 1,
         residuals_on_host=residuals_on_host,
         vjp_fn=new_vjp_fn,
     ), y
 
 
-def bwd_body(c: WrappedCarry, x_cotangent: jax.tree_util.PyTreeDef):
+def bwd_body(c: WrappedCarry, x_cotangents: jax.tree_util.PyTreeDef):
     # The vjp_fn here is constructed by the previous layer from host.
-    out_grad, layer_idx, residuals_on_host, vjp_fn = c
+    out_grad, slice_idx, residuals_on_host, vjp_fn = c
+    x_cotangent = x_cotangents
+
+    def _memcpy_from_host(x):
+        return annotate_memory_space(
+            jax.lax.dynamic_slice_in_dim(
+                annotate_memory_space(x, memory_space="host"),
+                slice_idx,
+                1,
+                allow_negative_indices=False,
+            ),
+            memory_space="device",
+        )
+
     out_grad, y_cotangent = vjp_fn((out_grad, x_cotangent))
     next_vjp_fn = jax.tree.map(
-        lambda x: jnp.squeeze(jax.lax.dynamic_slice_in_dim(x, layer_idx - 1, 1)),
+        lambda x: jnp.squeeze(_memcpy_from_host(x), axis=0),
         residuals_on_host,
     )
     return WrappedCarry(
         carry=out_grad,
-        layer_idx=layer_idx - 1,
+        slice_idx=slice_idx - 1,
         residuals_on_host=residuals_on_host,
         vjp_fn=next_vjp_fn,
     ), y_cotangent
@@ -67,6 +85,9 @@ def bwd_body(c: WrappedCarry, x_cotangent: jax.tree_util.PyTreeDef):
 def _pipelined_scan(
     fun: BodyFn, init: Carry, xs: X | None = None, length: int | None = None
 ):
+    assert length > 1, (
+        "When length is 1, we fallback to non-pipelined scan so this should not happen."
+    )
     return jax.lax.scan(fun, init, xs, length=length)
 
 
@@ -83,12 +104,11 @@ def _pipelined_scan_fwd(
         ),
         first_vjp_fn,
     )
-    layer_idx = 0
     final_fwd_carry, rest_ys = jax.lax.scan(
         partial(fwd_body, fun=fun),
         WrappedCarry(
             carry=out,
-            layer_idx=layer_idx + 1,
+            slice_idx=0,
             residuals_on_host=residuals_on_host,
             vjp_fn=first_vjp_fn,
         ),
@@ -104,13 +124,13 @@ def _pipelined_scan_fwd(
         if first_y is not None
         else None
     )
-    c, final_layer_idx, residuals_on_host, last_vjp_fn = final_fwd_carry
-    return (c, ys), (final_layer_idx, residuals_on_host, last_vjp_fn)
+    c, _, residuals_on_host, last_vjp_fn = final_fwd_carry
+    return (c, ys), (residuals_on_host, last_vjp_fn)
 
 
 def _pipelined_scan_bwd(fun: BodyFn, length: int, res, g):
     del fun
-    final_layer_idx, residuals_on_host, last_vjp_fn = res
+    residuals_on_host, last_vjp_fn = res
     c_cotangent, ys_cotangent = g
 
     first_y_cot = jax.tree.map(lambda x: x[0], ys_cotangent)
@@ -119,7 +139,7 @@ def _pipelined_scan_bwd(fun: BodyFn, length: int, res, g):
         bwd_body,
         WrappedCarry(
             carry=c_cotangent,
-            layer_idx=final_layer_idx - 1,
+            slice_idx=length - 2,
             residuals_on_host=residuals_on_host,
             vjp_fn=last_vjp_fn,
         ),
@@ -133,11 +153,10 @@ def _pipelined_scan_bwd(fun: BodyFn, length: int, res, g):
     c_cotangent, first_y_grad = first_vjp_fn((c_cotangent, first_y_cot))
 
     ys_grad = jax.tree.map(
-        lambda x, y: jnp.concatenate([x[None, :], y[::-1]], axis=0),
+        lambda x, y: jnp.concatenate([jnp.expand_dims(x, axis=0), y], axis=0),
         first_y_grad,
         rest_ys_grad,
     )
-    # return tuple(filter(lambda x: x is not None, (c_cotangent, ys_grad)))
     return c_cotangent, ys_grad
 
 
@@ -162,11 +181,15 @@ def pipelined_scan(
     reverse = kwargs.get("reverse", False)
     if reverse:
         raise NotImplementedError("Reverse pipelined scan is not implemented.")
-    unroll = kwargs.get("unroll", None)
-    if unroll is not None:
-        raise NotImplementedError("Unrolled pipelined scan is not implemented.")
     if length is None:
         assert xs is not None
         flattened, _ = jax.tree.flatten(xs)
         length = flattened[0].shape[0]
+    unroll = kwargs.get("unroll", 1)
+    needs_unroll = (isinstance(unroll, int) and unroll > 1) or (
+        isinstance(unroll, bool) and unroll
+    )
+    if length <= 1 or needs_unroll:
+        # Fallback to non-pipelined scan if length is 1.
+        return jax.lax.scan(fun, init, xs, length=length, unroll=unroll)
     return _pipelined_scan(fun, init, xs, length)
